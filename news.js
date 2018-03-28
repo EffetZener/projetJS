1dcbac05cc3bcdd17ab42488fa738d9f0e6ef66e
LEBOUCHER Nathan <lebouchn@hostname.iut2.upmf-grenoble.fr>
2018-03-28 17:41:24 +0200

    ajax fonctionnel

--- news.js
+++ news.js
@@ -2,6 +2,20 @@ var recherches=[];//tableau contenant des chaines de caracteres correspondant au
 var recherche_courante;// chaine de caracteres correspondant a la recherche courante
 var recherche_courante_news=[]; // tableau d'objets de type resultats (avec titre, date et url)
 
+function init()
+{/*
+    if(document.cookie){
+        var cookie = getCookie("recherches");
+            var array = JSON.parse(cookie);
+            for (var i=0; i<array.length;i++){
+                recherches.push(array[i]);
+                var query = "<p class=\"titre-recherche\"><label onclick=\"selectionner_recherche(this)\">"+array[i]+"</label><img src=\"croix30.jpg\" class=\"icone-croix\" onclick=\"supprimer_recherche(this)\"/> </p>";
+                $('#recherches-stockees').html($('#recherches-stockees').html() += query);
+            }
+    
+      }*/
+}
+
 function ajouter_recherche()
 {
     if (recherches.indexOf($("#zone_saisie").val()) == -1){
@@ -28,47 +42,24 @@ function selectionner_recherche(e)
 	$('#zone_saisie').val(recherche_courante);
 }
 
-
-function init()
-{
-    if(document.cookie){
-        var cookie = getCookie("recherches");
-            var array = JSON.parse(cookie);
-            for (var i=0; i<array.length;i++){
-                recherches.push(array[i]);
-                var query = "<p class=\"titre-recherche\"><label onclick=\"selectionner_recherche(this)\">"+array[i]+"</label><img src=\"croix30.jpg\" class=\"icone-croix\" onclick=\"supprimer_recherche(this)\"/> </p>";
-                $('#recherches-stockees').html($('#recherches-stockees').html() += query);
-            }
-    
-      }
-}
-
-
-function rechercher_nouvelles()
-{
+function rechercher_nouvelles(){
     $("#result").html="";
-    $('#wait').style.display="block";
-    var url ="search.php?data="+encodeURIComponent($('#zone_saisie').val());
-    ajax_get_request(maj_resultats, url, false);
-	
-}
-
-function ajax_get_request(callback,url,async){
-    var xhr = new XMLHttpRequest();  
-    xhr.onreadystatechange = function() {
-      if ((xhr.readyState==4) && (xhr.status == 200)){
-        callback(xhr.responseText);
-      }
-    };
+    document.getElementById('wait').style.display="block";
+	$.ajax({
+		url : 'search.php',
+		type :'GET',
+		data : {data : $('#zone_saisie').val()},
+		success : maj_resultats
+	});
 }
 
 function maj_resultats(res)
 {
-    $('#wait').style.display = "none";
+    document.getElementById('wait').style.display = "none";
 	var search = JSON.parse(res);
 	for (var i=0; i<search.length; i++){
 		var url = decodeEntities(search[i].url);
-		$('resultats').html($('resultats').html + "<p class=\"titre_result\"><a class=\"titre_news\" href="+url+" target=\"_blank\">"+search[i].titre+"</a><span class=\"date_news\">"+format(search[i].date)+"</span><span class=\"action_news\" onclick=\"sauver_nouvelle(this)\"><img src=\"horloge15.jpg\"/></span></p>");
+		$('#resultats').html($('#resultats').html() + "<p class=\"titre_result\"><a class=\"titre_news\" href="+url+" target=\"_blank\">"+search[i].titre+"</a><span class=\"date_news\">"+format(search[i].date)+"</span><span class=\"action_news\" onclick=\"sauver_nouvelle(this)\"><img src=\"style/horloge15.jpg\"/></span></p>");
 	}
 	
 }
